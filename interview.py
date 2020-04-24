23725aeea70d58fec3e8a1997410af4cbf4bbbe4
Rajit Khanna <31427851+rkhanna19@users.noreply.github.com>
2020-04-23 23:27:15 -0400

    Delete interview.py

--- interview.py
+++
@@ -1,9 +0,0 @@
-def num_coins(cents):
-	if cents == 0:
-		return 0
-	coin_val = [25, 10, 5, 1]
-	for val in coin_val:
-		quot = cents // val
-		if quot > 0:
-			return quot + num_coins(cents % val)
-print(num_coins(31))
