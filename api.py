6c790d95fcbee237c45821e65cc6c4277808941a
Rajit Khanna <rkhanna23@gatech.edu>
2020-05-02 00:41:22 -0400

    uploaded the images

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from image_bot import ImageBot
 from time import sleep
 
@@ -6,19 +6,22 @@ app = Flask(__name__)
 
 foods = [
     {
-        'name' : "Chicken Tikka Masala",
-        'description' : "Chicken tikka masala is a dish of chunks of roasted marinated chicken in a spiced curry sauce.",
-        'chef' : ""
+        'name': 'Chicken Tikka Masala',
+        'description': 'Chicken tikka masala is a dish of chunks of roasted marinated chicken in a spiced curry sauce.',
+        'chef': '',
+        'image': 'https://cafedelites.com/wp-content/uploads/2018/04/Best-Chicken-Tikka-Masala-IMAGE-2.jpg'
     },
     {
-        'name' : "Butter Chicken",
-        'description' : "Butter chicken or makhan murg is a dish, originating in the Indian subcontinent, of chicken in a mildly spiced tomato sauce.",
-        'chef' : "Rajesh Khanna"
+        'name': 'Butter Chicken',
+        'description': 'Butter chicken or makhan murg is a dish, originating in the Indian subcontinent, of chicken in a mildly spiced tomato sauce.',
+        'chef': 'Rajesh Khanna',
+        'image': 'https://cafedelites.com/wp-content/uploads/2019/01/Butter-Chicken-IMAGE-64.jpg'
     },
     {
-        'name' : "Paneer Tikka",
-        'description' : "Paneer tikka is an Indian dish made from chunks of paneer marinated in spices and grilled in a tandoor.",
-        'chef' : "Rajesh Khanna"
+        'name': 'Paneer Tikka',
+        'description': 'Paneer tikka is an Indian dish made from chunks of paneer marinated in spices and grilled in a tandoor.',
+        'chef': 'Rajesh Khanna',
+        'image': 'https://www.cookwithmanali.com/wp-content/uploads/2015/07/Restaurant-Style-Recipe-Paneer-Tikka.jpg'
     }
 ]
 
